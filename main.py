5b56d2289465b4b2b9873d4416a9cdd613dd022f
Quan-Ng <QNguyen7#07@gmail.com>
2024-06-26 07:00:25 -0700

    ASSIG 2-2:DONE

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ def main():
     Use the same variables: celsius fahrenheit 
     ##################################################
     """
-
+    celsius = int(input('Enter Celsius: '))
+    farenheit = (1.8 * celsius) + 32
+    
+    print (f'Farenheit \t {farenheit: .2f}')
+    
     """
     ########################################
     # Do not delete the return statement
     ########################################
     """
-    return celsius, fahrenheit
+    return celsius, farenheit
 
 
 if __name__ == '__main__':
